5fa4c19ddf4245db145a88fac46fb70e7c82a3d0
Nicolas Fauchereau <Nicolas.Fauchereau@gmail.com>
2014-05-30 12:15:17 +1200

    initial commit

---
+++ examples/mapping/full_map_vincent.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+from datetime import datetime 
+import vincent 
+import folium 
+import pandas as pd 
+
+stations = pd.read_csv('../../data/stations_open_1950.csv')
+
+Samoa = folium.Map(location=[-13.9, -171.75], width=2000, height=1000, zoom_start=10, tiles='Mapbox', API_key='nicolasf.ic0ebom5')
+
+x = np.random.randn(len(stations))
+
+for i in range(len(stations)): 
+    dat = stations.ix[i]
+    val = x[i]
+    print(val)
+    lat = dat['latitude']
+    lon = dat['longitude']
+    name = dat['name_primary']
+    dat = dat[['station_no','name_primary','start_date','end_date']]
+    dat = pd.DataFrame(dat)
+    #text = "<!DOCTYPE html> <html> <body> " + dat.to_html(na_rep=' ', header=False, index=False).replace('\n','') + " </body> </html>"
+    text = "NAME: <b>%s</b><br>\
+            LAT: %4.2f<br>LON: %6.2f<br>\
+            VALUE: %4.2f" % (name, lat, lon, val)
+    #text = dat.to_html(na_rep=' ', header=False, index=False).replace('\n','')
+    if val < 0: 
+        Samoa.circle_marker(location=[lat, lon], fill_color='#0000FF', radius=-val*1500, line_color=None, popup=text)
+    else: 
+        Samoa.circle_marker(location=[lat, lon], fill_color='#FF0000', radius=val*1500, line_color=None, popup=text)
+
+Samoa.create_map(path='Samoa.html')                         
---
+++ src/clide.py
@@ -0,0 +1,270 @@
+# clidesc.py
+
+# PROJECT: CLIDESC
+# Author: Nicolas Fauchereau, NIWA
+# Date: March 2014
+# Access functions for the CLIDE database
+
+import os, sys
+from datetime import datetime
+from dateutil import parser
+import psycopg2
+import pandas as pd
+import pandas.io.sql as psql
+from matplotlib import pyplot as plt
+
+###########################################################################
+# Settings
+dbhost = 'localhost'
+#dbhost = 'clidesc.niwa.co.nz'
+
+###########################################################################
+# get the command line parameters
+# we add some testing to the length of the passed parameters first 
+# that is in order to allow stand-alone non standard scripts to be developed
+# (e.g. the ICU one ...)
+
+if len(sys.argv) == 5: 
+    base_path = sys.argv[1] # base path is the path where the outputs, progress files and figures are saved
+    station_list = sys.argv[2] # list of stations, assumes string is passed, and comma is the separator 
+    from_date = parser.parse(sys.argv[3]) # from date: transform into a datetime object 
+    from_date = from_date.strftime('%Y-%m-%d') # and format correctly just in case the date wasnt entered YYYY-MM-DD
+    to_date = parser.parse(sys.argv[4]) # to date: idem 
+    to_date = to_date.strftime('%Y-%m-%d')
+    station_no = station_list.split(',')[0] # assumes the first station is the 'primary' station
+
+# Manage the progress file
+progress_count = 0
+
+###########################################################################
+def clidesc_progress(base_path, percent): 
+    with open(os.path.join(base_path,'progress.txt'), 'w+') as f: 
+        f.write("%s\n" % ( percent ) )
+        f.close()
+
+def clidesc_progress_add(progress_count):
+    pass
+
+###########################################################################
+def clidesc_open(base_path):
+    """
+    OPEN the ClideDB database and the progress file 
+    """
+    clidesc_progress(base_path, 0)
+    try:
+        conn = psycopg2.connect(database="clideDB", user="clidegui", password="clidepass", host=dbhost)
+        clidesc_progress(base_path, 1)
+        return conn
+    except: 
+        print('\n\nERROR!: Unable to establish the connection to the database\n\n')
+
+################################################################
+def clidesc_stations(conn, station_no): 
+    """
+    gets all the station details for a station number
+    It opens the connection, reads the stations table
+    station_no is a string, with stations separated by ','
+    """
+
+    if isinstance(station_no, str) and ',' in station_no: 
+        station_no = station_no.replace(',','\',\'')
+
+    # builds the query string 
+    query = """SELECT * FROM stations WHERE station_no IN ('%s') ORDER BY station_no""" % (station_no)
+
+    # get the table returned by the query as a pandas dataframe 
+    table = psql.frame_query(query, conn)
+    return table
+
+################################################################
+def clidesc_getStationsByCountry(conn, country): 
+    """
+    gets all the station details for all stations in a given country code (single or vector).
+    It opens the connection, reads the stations table
+    country must be a string, with country codes (if more than one) separated by ','
+    """
+    
+    if ',' in country: 
+        country = country.replace(',','\',\'')
+    
+    # builds the query string
+    query = """SELECT * FROM stations WHERE country_code IN ('%s') ORDER BY country_code, station_no""" % ( country )
+    # get the table returned by the query as a pandas dataframe 
+    table = psql.frame_query(query, conn)
+    return table
+
+################################################################
+def clidesc_ObsDaily(conn, channels, stations, from_date, to_date):
+    """
+    returns the daily observations table for the requested stations, date range and columns
+    depends on an open clidDB connection clideDB
+    
+    Usage:
+    table = clidesc_ObsDaily(clideDB, channels, stations, from_date, to_date )
+    Currently no parameter checking.
+    """
+    
+    # some string formatting on the stations if more than one
+    if isinstance(stations, str) and ',' in stations:
+        stations = stations.replace(',','\',\'')
+    
+    inputs = '%s::%s::%s::%s' % (channels, stations, from_date, to_date)
+    
+    query = """
+    SELECT station_no, TO_CHAR(lsd, 'yyyy-mm-dd') as LSD, 
+    %s FROM obs_daily WHERE station_no IN ('%s') AND lsd >= TO_TIMESTAMP('%s', 'yyyy-mm-dd') 
+    AND lsd <= TO_TIMESTAMP('%s', 'yyyy-mm-dd') ORDER BY lsd""" % (channels, stations, from_date, to_date)
+
+    # get the table returned by the query as a pandas dataframe 
+    try: 
+        table = psql.frame_query(query, conn)
+    except: 
+        print('query failed for %s, was probably malformed' % (inputs))
+    # the index of the pandas DataFrame is the 'lsd' field, correctly 
+    # cast to pandas datetime index and named 'timestamp'
+    table.index = pd.to_datetime(table.lsd)
+    table.index.name = 'timestamp'
+    
+    return table
+
+################################################################
+def clidesc_rain24h(conn, stations, from_date, to_date):
+    """
+    Utility function to read the rain_24h channel from the obs_daily table 
+    """
+    return clidesc_ObsDaily(conn, 'rain_24h', stations, from_date, to_date)
+
+################################################################
+def clidesc_ObsSubDaily(conn, channels, stations, from_date, to_date):
+    """
+    Read channels for stations (stations) from the obs_subdaily table
+    usage: 
+    table = clidesc_ObsSubDaily(clideDB, channels, stations, from_date, to_date )
+    """
+    
+    inputs = '%s::%s::%s::%s' % (channels, stations, from_date, to_date)
+    
+    # some string formatting on the stations if more than one
+    if isinstance(stations, str) and ',' in stations:
+        stations = stations.replace(',','\',\'')
+    
+    # builds the query string
+    query = """SELECT station_no
+    , TO_CHAR(lsd, 'yyyy-mm-dd') as LSD
+    , %s
+    FROM obs_subdaily AS tab
+    WHERE station_no IN ('%s')
+    AND lsd >= TO_TIMESTAMP('%s', 'yyyy-mm-dd')
+    AND lsd <= TO_TIMESTAMP('%s', 'yyyy-mm-dd')
+    ORDER BY lsd""" % (channels, stations, from_date, to_date)
+
+    # get the table returned by the query as a pandas dataframe 
+    try: 
+        table = psql.frame_query(query, conn)
+    except: 
+        print('query failed for %s, was probably malformed' % (inputs))
+    # the index of the pandas DataFrame is the 'lsd' field, correctly 
+    # cast to pandas datetime index and named 'timestamp'
+    table.index = pd.to_datetime(table.lsd)
+    table.index.name = 'timestamp'
+    
+    return table
+
+################################################################
+def clidesc_Obs(conn, table, channels, stations, from_date, to_date):
+    """
+    returns the given observations table for the requested stations, date range and columns
+    depends on an open clidDB connection clideDB
+
+    Usage:
+    table = clidesc_Obs(conn, "obs_subdaily", channels, stations, from_date, to_date)
+    Currently no parameter checking.
+    """
+
+    # some string formatting on the stations if more than one
+    if isinstance(stations, str) and ',' in stations:
+        stations = stations.replace(',','\',\'')
+
+    inputs = '%s::%s::%s::%s::%s' % (channels, table, stations, from_date, to_date)
+    
+    query = """SELECT station_no
+    , TO_CHAR(lsd, 'yyyy-mm-dd') as LSD
+    , %s
+    FROM %s AS tab
+    WHERE station_no IN ('%s')
+    AND lsd >= TO_TIMESTAMP('%s', 'yyyy-mm-dd')
+    AND lsd <= TO_TIMESTAMP('%s', 'yyyy-mm-dd')
+    ORDER BY lsd""" % (channels, table, stations, from_date, to_date)
+
+    # get the table returned by the query as a pandas dataframe 
+    try: 
+        data = psql.frame_query(query, conn)
+    except: 
+        print('query failed for %s, was probably malformed' % (inputs))
+    # the index of the pandas DataFrame is the 'lsd' field, correctly 
+    # cast to pandas datetime index and named 'datetime'
+    data.index = pd.to_datetime(data.lsd)
+    data.index.name = 'timestamp'
+    
+    return data
+
+################################################################
+def cm2inch(*tupl):
+    """
+    A little function that converts cm in inches
+    It is used in 'clidesc_Figure'
+    """
+    inch = 2.54
+    if type(tupl[0]) == tuple:
+        return tuple(i/inch for i in tupl[0])
+    else:
+        return tuple()
+
+
+################################################################
+def clidesc_Figure(size='A4'):
+    """
+    Initializes figures (f) and axes (ax) with correct 
+    figure size and dpi value for: 
+        'A4' (default)
+        'A5'
+        '16/12in'
+    """
+    if size == 'A4':
+        figsize = cm2inch((29.7,21.0))
+        f, ax = plt.subplots(figsize=figsize, dpi=300)
+    elif size == 'A5':
+        figsize = cm2inch((29.7,21.0))
+        f, ax = plt.subplots(figsize=figsize, dpi=150)
+    elif size == '16/12in':
+        figsize = cm2inch((16,12))
+        f, ax = plt.subplots(figsize=figsize, dpi=75)
+    return f, ax
+
+################################################################
+def make_thumbnail(srcfile, thumbfile, height, width):
+    # makeThumb 
+    # creates a thumbnail of the given size from the srcFile and 
+    # puts it into the thumbfile.  Expects a PNG. 
+
+    #     clidesc_makeThumb <- function(srcfile, thumbfile, height, width) {
+    #     require(png)
+    #     img <- readPNG(srcfile)
+    #     png(file = thumbfile, height = height, width = width)
+    #     par(mar=c(0,0,0,0), xaxs="i", yaxs="i", ann=FALSE)
+    #     plot(1:2, type='n', xaxt = "n", yaxt = "n", xlab = "", ylab = "") 
+    #     lim <- par()
+    #     rasterImage(img, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4])
+    #     dev.off()
+    pass
+
+
+################################################################
+# CLOSE
+# Closes the connection && Frees all the resources on the driver
+
+def clidesc_close(base_path, conn):
+    """docstring for clides"""
+    clidesc_progress(base_path, 100)
+    if conn.closed == 0: 
+        conn.close()
