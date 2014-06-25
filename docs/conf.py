f966973b83de9d2680321f1266e5b4e0adc79102
Nicolas Fauchereau <Nicolas.Fauchereau@gmail.com>
2014-06-25 16:46:13 +1200

    try include modules

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,7 @@ html_short_title = 'CLIDEsc'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'clide.png'
+#html_logo = 'clide.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
