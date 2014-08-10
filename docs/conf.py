8bc8e86c105a9adb81c92ab20813fc66ba6c80ec
Nicolas Fauchereau <nicolas.fauchereau@gmail.com>
2014-08-11 09:52:33 +1200

    installed the numpydoc extension to sphinx and updated the conf.py file

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'numpydoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -120,7 +121,7 @@ html_theme = 'haiku'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'CLIDEsc documentation' 
+html_title = 'CLIDEsc documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = 'CLIDEsc'
