cd108023643c3a9de79e758eebde2fca100b8536
Nicolas Fauchereau <nicolas.fauchereau@gmail.com>
2014-08-13 11:35:08 +1200

    fixed progress.txt bug

--- clidesc/clide.py
+++ clidesc/clide.py
@@ -34,8 +34,7 @@ progress_count = 0
 def clidesc_progress(base_path, percent):
     with open(os.path.join(base_path,'progress.txt'), 'w+') as f:
         f.write("{}".format(percent))
-        f.close()
-
+        
 ###########################################################################
 def clidesc_open(base_path, database="clideDB", user="XXX", password="XXX", dbhost='localhost'):
     """
